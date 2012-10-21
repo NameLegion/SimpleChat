c96fd56980e08423a945968247ad3ae6024b0efe
Anton <tank-gun@mail.ru>
2012-10-21 23:52:22 +0300

    v 0.1
    interface

---
+++ SimpleChat/SimpleChat/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace SimpleChat
+{
+    partial class FormForEnter
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButtonClient = new System.Windows.Forms.RadioButton();
+            this.radioButtonServer = new System.Windows.Forms.RadioButton();
+            this.Enter = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxUrl = new System.Windows.Forms.TextBox();
+            this.textBoxNick = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // radioButtonClient
+            // 
+            this.radioButtonClient.AutoSize = true;
+            this.radioButtonClient.Location = new System.Drawing.Point(120, 72);
+            this.radioButtonClient.Name = "radioButtonClient";
+            this.radioButtonClient.Size = new System.Drawing.Size(61, 17);
+            this.radioButtonClient.TabIndex = 0;
+            this.radioButtonClient.TabStop = true;
+            this.radioButtonClient.Text = "Клиент";
+            this.radioButtonClient.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonServer
+            // 
+            this.radioButtonServer.AutoSize = true;
+            this.radioButtonServer.Location = new System.Drawing.Point(120, 95);
+            this.radioButtonServer.Name = "radioButtonServer";
+            this.radioButtonServer.Size = new System.Drawing.Size(62, 17);
+            this.radioButtonServer.TabIndex = 1;
+            this.radioButtonServer.TabStop = true;
+            this.radioButtonServer.Text = "Сервер";
+            this.radioButtonServer.UseVisualStyleBackColor = true;
+            // 
+            // Enter
+            // 
+            this.Enter.Location = new System.Drawing.Point(241, 128);
+            this.Enter.Name = "Enter";
+            this.Enter.Size = new System.Drawing.Size(75, 23);
+            this.Enter.TabIndex = 2;
+            this.Enter.Text = "Next";
+            this.Enter.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "URL";
+            // 
+            // textBoxUrl
+            // 
+            this.textBoxUrl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxUrl.Location = new System.Drawing.Point(81, 6);
+            this.textBoxUrl.Name = "textBoxUrl";
+            this.textBoxUrl.Size = new System.Drawing.Size(235, 26);
+            this.textBoxUrl.TabIndex = 4;
+            // 
+            // textBoxNick
+            // 
+            this.textBoxNick.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxNick.Location = new System.Drawing.Point(81, 38);
+            this.textBoxNick.Name = "textBoxNick";
+            this.textBoxNick.Size = new System.Drawing.Size(235, 26);
+            this.textBoxNick.TabIndex = 6;
+            this.textBoxNick.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Nick";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // FormForEnter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(328, 163);
+            this.Controls.Add(this.textBoxNick);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxUrl);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Enter);
+            this.Controls.Add(this.radioButtonServer);
+            this.Controls.Add(this.radioButtonClient);
+            this.Name = "FormForEnter";
+            this.Text = "Entering";
+            this.Load += new System.EventHandler(this.FormForEnter_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButtonClient;
+        private System.Windows.Forms.RadioButton radioButtonServer;
+        private System.Windows.Forms.Button Enter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxUrl;
+        private System.Windows.Forms.TextBox textBoxNick;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ SimpleChat/SimpleChat/Form1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SimpleChat
+{
+    public partial class FormForEnter : Form
+    {
+        public FormForEnter()
+        {
+            InitializeComponent();
+        }
+    }
+}
